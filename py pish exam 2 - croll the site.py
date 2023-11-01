b51bebcf0cbb11a57f6f1006f6a4cb8d88d198eb
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-11-01 15:09:58 +0330

    Update py pish exam 2 - croll the site.py
    add title
    add view
    add tags
    in print test

--- py pish exam 2 - croll the site.py
+++ py pish exam 2 - croll the site.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 # for i in range(1974776,1975776):
-url = f"https://www.varzesh3.com/news/{1974786}"
+url = f"https://www.varzesh3.com/news/{1974743}"
 res = requests.get(url).text
 res_b = BeautifulSoup(res , "html.parser")
 ##################################################################
 all_title = res_b.find("div" , class_ = "news-detail-text")
 
 title = str(all_title.find("h1" , class_ = "headline")).replace('<h1 class="headline">' , '' ).replace("</h1>","")
-# print("title : ",title)
+print("title : ",title)
 
-view = int(str(all_title.find("div", class_ = "news-info")).split("<span>")[3].replace("| </span>","").replace(" بازدید", "").replace("K",""))*1000
-# print("view : ",view)
+view = int(float(str(all_title.find("div", class_ = "news-info")).split("<span>")[3].replace("| </span>","").replace(" بازدید", "").replace("K",""))*1000)
+print("view : ",view)
 ####################################################################
 
 all_tags = str(res_b.find("div", class_ = "tags tags-news").find_all("span")).replace("<span>","").replace("</span>","").replace("[","").replace("]","")
 print("tags for this news: ",all_tags)
+
+
+
+
