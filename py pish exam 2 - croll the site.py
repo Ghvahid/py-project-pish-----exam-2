42a7996f15c7b9ca75d69ead4dac9a5b3163cc0d
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-11-01 15:38:54 +0330

    Update py pish exam 2 - croll the site.py
    Update  and Develop code

--- py pish exam 2 - croll the site.py
+++ py pish exam 2 - croll the site.py
@@ -6,13 +6,14 @@ file = open("VarzeshNews.csv", "w", encoding="UTF-8", newline="")
 writer = csv.writer(file)
 
 list_all = []
-for i in range(1922457,1923457):
+selected_news_code = 1976282
+for i in range(selected_news_code,selected_news_code+1000):
     inner_list = []
     url = f"https://www.varzesh3.com/news/{i}"
     res = requests.get(url).text
     res_b = BeautifulSoup(res , "html.parser")
-
-    all_title = res_b.find("div" , class_ = "news-detail-text")
+    print(i)
+    all_title = res_b.find("div", class_ = "news-detail-text")
     title = str(all_title.find("h1", class_="headline")).replace('<h1 class="headline">', '').replace("</h1>","")
     inner_list.append(title)
     # print("title : ",title)
@@ -26,6 +27,7 @@ for i in range(1922457,1923457):
     all_tags = str(res_b.find("div", class_ = "tags tags-news").find_all("span")).replace("<span>","").replace("</span>","").replace("[","").replace("]","")
     inner_list.append(all_tags)
     # print("tags for this news: ",all_tags)
+
     list_all.append(inner_list)
 
 
@@ -33,5 +35,5 @@ writer.writerow(['title', 'view', 'tags'])
 writer.writerows(list_all)
 file.close()
 
-print(list_all)
+# print(list_all)
 
