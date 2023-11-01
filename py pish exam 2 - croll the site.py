303ce76de115e5b37712b8a041f9602257a5eb96
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-11-01 15:20:04 +0330

    save in VarzeshNews.csv

--- py pish exam 2 - croll the site.py
+++ py pish exam 2 - croll the site.py
@@ -2,35 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# file = open("new3.csv", "w", encoding="UTF-8", newline="")
-# writer = csv.writer(file)
+file = open("VarzeshNews.csv", "w", encoding="UTF-8", newline="")
+writer = csv.writer(file)
 
 list_all = []
 for i in range(1974776,1974785):
-    l = []
+    inner_list = []
     url = f"https://www.varzesh3.com/news/{i}"
     res = requests.get(url).text
     res_b = BeautifulSoup(res , "html.parser")
-    ##################################################################
-    all_title = res_b.find("div" , class_ = "news-detail-text")
 
-    title = str(all_title.find("h1" , class_ = "headline")).replace('<h1 class="headline">' , '' ).replace("</h1>","")
-    l.append(title)
+    all_title = res_b.find("div" , class_ = "news-detail-text")
+    title = str(all_title.find("h1", class_="headline")).replace('<h1 class="headline">', '').replace("</h1>","")
+    inner_list.append(title)
     # print("title : ",title)
 
+
     view = int(float(str(all_title.find("div", class_ = "news-info")).split("<span>")[3].replace("| </span>","").replace(" بازدید", "").replace("K",""))*1000)
-    l.append(view)
+    inner_list.append(view)
     # print("view : ",view)
-    ####################################################################
+
 
     all_tags = str(res_b.find("div", class_ = "tags tags-news").find_all("span")).replace("<span>","").replace("</span>","").replace("[","").replace("]","")
-    l.append(all_tags)
+    inner_list.append(all_tags)
     # print("tags for this news: ",all_tags)
-    list_all.append(l)
+    list_all.append(inner_list)
 
-# writer.writerow( ['a', 'b', 'c'] )
-# writer.writerows( list_all )
-# file.close()
+
+writer.writerow(['title', 'view', 'tags'])
+writer.writerows(list_all)
+file.close()
 
 print(list_all)
 
